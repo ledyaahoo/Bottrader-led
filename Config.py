eb68cabca5967ce4aaf97e3281c0ffe39381a9b1
ledyaahoo <ledyaahoo@gmail.com>
2025-11-18 12:04:47 +0700

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,7 +1,7 @@
 import os
 
 # =========================
-# API Settings
+# API Settings (Secret dari Environment)
 # =========================
 BITGET_API_KEY = os.getenv("BITGET_API_KEY")
 BITGET_API_SECRET = os.getenv("BITGET_API_SECRET")
@@ -23,32 +23,27 @@ MEME_PAIRS = ["PEPEUSDT", "SHIBUSDT", "FLOKIUSDT", "WIFUSDT", "BONKUSDT", "AVNTU
 # =========================
 # Leverage
 LEVERAGE_NORMAL = 25
-LEVERAGE_NORMAL_SECOND_DAY = 20
-LEVERAGE_SNIPER_SMALL = 13 #<0.5
-LEVERAGE_SNIPER_BIG = 20 #>=0.5
+LEVERAGE_SNIPER_SMALL = 13
+LEVERAGE_SNIPER_BIG = 20
+LEVERAGE_SNIPER_BIG_MAX = 25
 
 # =========================
 # Target Profit
 NORMAL_TARGET_INITIAL = 30
+NORMAL_MULTIPLIER = 3
 SNIPER_TARGET_INITIAL = 40
-MAX_PROFIT = 3000
+SNIPER_MULTIPLIER = 1.5
+MAX_PROFIT = 3000  # setelah profit > 3000, target = x1
 
 # =========================
 # Orders
-# =========================
 MAX_ORDER_INITIAL = 5  # 3 normal + 2 sniper
 MAX_ORDER_TARGET2 = 6
-MAX_ORDER_SCALE = 8
-
-#=========================
-# Margin Allocation
-# =========================
-NORMAL_MARGIN_RATIO = 0.4  # jika target hari kedua tercapai
-SNIPER_MARGIN_RATIO = 0.3
-RESERVE_MARGIN_RATIO = 0.3
-
-# =========================
-# Execution / Delta
-# =========================
 DELTA_BUFFER = 0.03
 FAST_MODE = 0.3  # 0.3 detik lebih cepat dari ritel
+
+# =========================
+# Margin Allocation
+NORMAL_MARGIN_RATIO = 0.4
+SNIPER_MARGIN_RATIO = 0.3
+RESERVE_MARGIN_RATIO = 0.3
